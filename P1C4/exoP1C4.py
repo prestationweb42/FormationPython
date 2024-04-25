c544625928b62a41e0c67797a539dab03e9c00a1
prestationweb42 <dani.vent@orange.fr>
2024-04-25 11:58:25 +0200

    Finish lesson P1C4

---
+++ P1C4/exoP1C4.py
@@ -0,0 +1,25 @@
+# Les variables
+livre = "Gatsby le Magnifique"
+print(livre)
+
+age = 25
+print(age)
+
+# Modifier une variable
+livre = "Le nom de la rose"
+print(livre)
+
+# Afficher avec f-string
+nom = "Dupont"
+prenom = "Jean"
+age = 30
+
+print(f"Bonjour, je m'appelle {prenom} {nom} et j'ai {age} ans.")
+
+# Exercices
+nom = "Dan"
+age = 35
+print(f"Bonjour, je m'appelle {nom} et j'ai {age} ans.")
+# Modification de l'âge
+age += 10 
+print(f"Bonjour, je m'appelle {nom} et j'ai {age} ans maintenant.")
