9c7d24911f1b20f89699ffaf5bfa5028551f168f
prestationweb42 <dani.vent@orange.fr>
2024-04-26 08:25:14 +0200

    Finish lesson P1C5

---
+++ P1C5/exoP1C5.py
@@ -0,0 +1,9 @@
+# Classez des données avec les types de données
+nom = 'Dan'
+age = 35
+taille = 1.90
+est_etudiant = True
+
+print(f'Votre nom est {nom} et vous avez {age} ans')
+print(f'Votre nom est {nom} et de type : {type(nom)}')
+print(f'Vous avez {age} ans et de type : {type(age)}')
